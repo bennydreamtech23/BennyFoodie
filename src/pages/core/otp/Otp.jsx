dea87409efbf9877669c9acc7be1e55aab583b9a
ikechukwu11 <kevinuzoma@gmail.com>
2023-02-18 12:32:53 +0100

    Reset Password

--- src/pages/core/otp/Otp.jsx
+++ src/pages/core/otp/Otp.jsx
@@ -1,164 +1,144 @@
-import {Container, 
-Col, 
-Form,
-InputGroup,
-Row , 
-Toast,
-ToastContainer} from 'react-bootstrap';
-import {useNavigate, Link, useLocation} from "react-router-dom";
-import styles from "./Otp.module.scss";
- import { Formik } from 'formik';
- import * as Yup from 'yup';
- import {useState, useEffect} from "react"
-
 import {
-  MdErrorOutline, 
- } from "react-icons/md";
+  Container,
+  Col,
+  Form,
+  InputGroup,
+  Row,
+  Toast,
+  ToastContainer,
+} from "react-bootstrap";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import styles from "./Otp.module.scss";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useState, useEffect } from "react";
 
+import { MdErrorOutline } from "react-icons/md";
 
 function OtpPage() {
-const navigate = useNavigate()
-const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const [showToast, setShowToast] = useState(false)
-  const [errorType, setErrorType] = useState('')
-  const [messageType, setMessageType] = useState('')
-  const [token, setToken] = useState(location?.state?.token)
-    const [userId, setUserId] = useState(location?.state?.userId)
+  const [showToast, setShowToast] = useState(false);
+  const [errorType, setErrorType] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const [token, setToken] = useState(location?.state?.token);
+  const [userId, setUserId] = useState(location?.state?.userId);
 
-
-useEffect(() => {
+  useEffect(() => {
     if (token && userId) {
-      setToken(token)
-      setUserId(userId)
+      setToken(token);
+      setUserId(userId);
     }
-  }, [token, userId])
+  }, [token, userId]);
 
-
-const handleSignup = (e) =>{
-e.preventDefault()
-navigate('/signup')
-}
+  const handleSignup = (e) => {
+    e.preventDefault();
+    navigate("/signup");
+  };
 
   return (
     <Container fluid className={styles.Container}>
-           <h1 className={styles.title}> Forgot password </h1>
-           <p className={styles.subTitle}>
-         Enter your OTP sent to your mail
-           </p>
-    
-     {/*registration form*/} 
+      <h1 className={styles.title}> Forgot password </h1>
+      <p className={styles.subTitle}>Enter your OTP sent to your mail</p>
+
+      {/*registration form*/}
       <Formik
-       initialValues={{ 
-       otp: '',
-       }}
-  
-       validationSchema={Yup.object({
-         otp: Yup.string()
-            .required('No OTP provided.')
-            .min(6, 'OTP Token is too short - should be 4 chars minimum.'),
-       })}
-       onSubmit={() => {
+        initialValues={{
+          otp: "",
+        }}
+        validationSchema={Yup.object({
+          otp: Yup.string()
+            .required("No OTP provided.")
+            .min(6, "OTP Token is too short - should be 4 chars minimum."),
+        })}
+        onSubmit={() => {
           //e.preventDefault()
-          fetch(`https://benny-foods.fly.dev/api/v1/users/reset-password/validate-token?token=${token}&user_id=${userId}`,
-          {
-            method: 'POST',
-            //headers: {
-            //'Content-Type': 'application/json',
-            //'Accept': 'application/json'
-            // },
-           body: JSON.stringify({
-            otp: otp.value,
-           }),
-          })
+          fetch(
+            `https://benny-foods.fly.dev/api/v1/users/reset-password/validate-token?token=${token}&user_id=${userId}`,
+            {
+              method: "POST",
+              //headers: {
+              //'Content-Type': 'application/json',
+              //'Accept': 'application/json'
+              // },
+              body: JSON.stringify({
+                otp: otp.value,
+              }),
+            }
+          )
             .then((response) => response.json())
             .then((data) => {
-               if(otp === token){
-         //  if (data.status === 'success') {
-             //   alert(data.message)
-               // setErrorType('success')
-               // setMessageType(data.message)
-               // setShowToast(true)
-             //navigate('/resetpassword', { replace: true })
-        // }
-        alert('true')
-             }
-           else {
-               // alert(data.error.description)
-                setErrorType('danger')
-               setMessageType(data.error.description)
-                setShowToast(true)
+              if (data.status === "success") {
+                alert(data.message);
+                navigate("/resetpassword", {state: {userId: userId }}, { replace: true });
+              } else {
+                // alert(data.error.description)
+                setErrorType("danger");
+                setMessageType(data.error.description);
+                setShowToast(true);
               }
             })
-            .catch((error) => console.log(error))
-        }}>
-       {formik => (
-         <Form  noValidate
-         autoComplete='off'
-         onSubmit={formik.handleSubmit}>
-         
-{/*email section of the page*/} 
+            .catch((error) => console.log(error));
+        }}
+      >
+        {(formik) => (
+          <Form noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
+            {/*email section of the page*/}
 
-          <Form.Group className={styles.group}>
-         <div className={styles.subgroup}> 
-    
-      <Form.Label className={styles.labelfield}>
-OTP
-      </Form.Label>
-      
-          <InputGroup className={styles.inputField}>
-          
-              <Form.Control
-                 size="md" 
-            type="number"
-          name="otp"
-     placeholder="Enter your OTP Value"
-            id="otp"
-      className={styles.inputbox} 
-             {...formik.getFieldProps('otp')}
-           />
-               </InputGroup>
-    {formik.touched.otp && formik.errors.otp ? (
-             <div className={styles.errorMsg}> <MdErrorOutline className="h6 mt-1"/> {formik.errors.otp}</div>
-           ) : null}      
-        
-</div>
-     </Form.Group>
-     
-           <button className={styles.loginButton} 
-           type="submit">
-          Continue
-           </button>
-         </Form>
-       )}
-     </Formik>
-     
-     <hr/>
-     <div className={styles.semiGroup}>
-     <p className="text-center">
-      Don't Have an Account
-     </p>
-     <button className={styles.btnLink} onClick={handleSignup}>Signup</button>
-     </div>
-    
-        <Toast
-          bg={errorType}
-          show={showToast}
-          onClose={() => {
-            setShowToast(!showToast)
-          }}
-           className={styles.toaster}
-        >
-          <Toast.Header>
-            <img
-              src='holder.js/20x20?text=%20'
-              className='rounded me-2'
-              alt=''
-            />
-            <strong className='me-auto'>BennyFoodie</strong>
-          </Toast.Header>
-          <Toast.Body className='text-white'>{messageType}</Toast.Body>
-        </Toast>
+            <Form.Group className={styles.group}>
+              <div className={styles.subgroup}>
+                <Form.Label className={styles.labelfield}>OTP</Form.Label>
+
+                <InputGroup className={styles.inputField}>
+                  <Form.Control
+                    size="md"
+                    type="number"
+                    name="otp"
+                    placeholder="Enter your OTP Value"
+                    id="otp"
+                    className={styles.inputbox}
+                    {...formik.getFieldProps("otp")}
+                  />
+                </InputGroup>
+                {formik.touched.otp && formik.errors.otp ? (
+                  <div className={styles.errorMsg}>
+                    {" "}
+                    <MdErrorOutline className="h6 mt-1" /> {formik.errors.otp}
+                  </div>
+                ) : null}
+              </div>
+            </Form.Group>
+
+            <button className={styles.loginButton} type="submit">
+              Continue
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      <hr />
+      <div className={styles.semiGroup}>
+        <p className="text-center">Don't Have an Account</p>
+        <button className={styles.btnLink} onClick={handleSignup}>
+          Signup
+        </button>
+      </div>
+
+      <Toast
+        bg={errorType}
+        show={showToast}
+        onClose={() => {
+          setShowToast(!showToast);
+        }}
+        className={styles.toaster}
+      >
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">BennyFoodie</strong>
+        </Toast.Header>
+        <Toast.Body className="text-white">{messageType}</Toast.Body>
+      </Toast>
     </Container>
   );
 }
--- src/pages/core/reset/Reset.jsx
+++ src/pages/core/reset/Reset.jsx
@@ -1,202 +1,211 @@
-import {Container, 
-Col, 
-Form,
-InputGroup,
-Row , 
-Toast,
-ToastContainer} from 'react-bootstrap';
-import {useNavigate} from "react-router-dom";
+import {
+  Container,
+  Col,
+  Form,
+  InputGroup,
+  Row,
+  Toast,
+  ToastContainer,
+} from "react-bootstrap";
+import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./Reset.module.scss";
- import { Formik } from 'formik';
- import * as Yup from 'yup';
- import {useState} from "react"
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useState, useEffect } from "react";
 
 //icon
 import { RiLockPasswordFill } from "react-icons/ri";
-import {
-  MdOutlinePhoneLocked, 
-  MdErrorOutline, 
-  } from "react-icons/md";
+import { MdOutlinePhoneLocked, MdErrorOutline } from "react-icons/md";
 
-import {
-  AiOutlineEyeInvisible, 
-AiOutlineEye
-} from "react-icons/ai";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 
 function ResetPage() {
-  const [showToast, setShowToast] = useState(false)
-  const [errorType, setErrorType] = useState('')
-  const [messageType, setMessageType] = useState('')
-  //toggle
-  const [togglePassword, setTogglePassword] = useState(false)
-  const [toggleConfirmPassword, setToggleConfirmPassword] = useState(false)
-  
-const navigate = useNavigate()
+  const location = useLocation();
 
-const handleSignup = (e) =>{
-e.preventDefault()
-navigate('/signup')
-}
+  const [token, setToken] = useState(location?.state?.token);
+  const [userId, setUserId] = useState(location?.state?.userId);
+  const [showToast, setShowToast] = useState(false);
+  const [errorType, setErrorType] = useState("");
+  const [messageType, setMessageType] = useState("");
+  //toggle
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [toggleConfirmPassword, setToggleConfirmPassword] = useState(false);
+
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userId) {
+      setUserId(userId);
+      console.log(userId);
+    } else {
+      navigate("/forgotpassword")
+    }
+  }, [token, userId]);
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    navigate("/signup");
+  };
 
   return (
     <Container fluid className={styles.Container}>
-           <h1 className={styles.title}>Welcome Back to <span className={styles.color}>Bennyfoodie!</span></h1>
-           <p className={styles.subTitle}>
-         Enter your details, our prominent Customers
-           </p>
-    
-     {/*registration form*/} 
-      <Formik
-       initialValues={{ 
-        password: '',
-        confirmPassword:''
-       }}
-  
-       validationSchema={Yup.object({
-       
-    password: Yup.string()
-  .required('No new password provided.')
-  .min(8, 'new Password is too short - should be 8 chars minimum.')
-  .matches(/[a-zA-Z]/, 'new Password can only contain Latin letters.'),
-   confirmPassword: Yup.string().when("password", {
-    is: val => (val && val.length > 0 ? true : false),
-    then: Yup.string().oneOf(
-      [Yup.ref("password")],
-      "New Password Must Match"
-    )
-  })
-  
-       })}
-       onSubmit={(values) => {
-        navigate('/login')
-        }}
+      <h1 className={styles.title}>
+        Welcome Back to <span className={styles.color}>Bennyfoodie!</span>
+      </h1>
+      <p className={styles.subTitle}>
+        Enter your details, our prominent Customers
+      </p>
 
-     >
-       {formik => (
-         <Form  noValidate
-         autoComplete='off'
-         onSubmit={formik.handleSubmit}>
-         
-    {/*pasword section*/}  
-      <Form.Group className={styles.group}>
-     
-     <div className={styles.subgroup}> 
-    
-      <Form.Label className={styles.labelfield}>
-Password
-      </Form.Label>
-      
-  <InputGroup className={styles.inputField}>
-         <InputGroup.Text id="inputGroupPrepend">
-        <RiLockPasswordFill/></InputGroup.Text>
-                 
-              <Form.Control
-          size="md" 
-          type={togglePassword ? 'text' : 'password'}
-                name="password"
-                placeholder="Enter your new password"
-              id="password"
-           className={styles.inputbox} 
-             {...formik.getFieldProps('password')}
-           />
-        <InputGroup.Text id='inputGroupPrepend'  onClick={(e) => {
-                    e.preventDefault()
-                    setTogglePassword(!togglePassword)
-                  }}>
-                  {togglePassword ? (
-                    <AiOutlineEyeInvisible />
-                  ) : (
-                    <AiOutlineEye />
-                  )}
-                </InputGroup.Text>
-      
-               </InputGroup>
-               
-       {formik.touched.password && formik.errors.password ? (
-             <div className={styles.errorMsg}><MdErrorOutline className="h6 mt-1"/> {formik.errors.password}</div>
-           ) : null}         
-         
-</div>
-     </Form.Group>
-     
-       {/*confirm pasword section*/}  
-      <Form.Group className={styles.group}>
-     
-     <div className={styles.subgroup}> 
-    
-      <Form.Label className={styles.labelfield}>
-   Confirm Password
-      </Form.Label>
-      
-  <InputGroup className={styles.inputField}>
-         <InputGroup.Text id="inputGroupPrepend">
-        <RiLockPasswordFill/>
-        </InputGroup.Text>
-                 
-              <Form.Control
-          size="md" 
-          type={toggleConfirmPassword ? 'text' : 'password'}
-                name="confirmPassword"
-                placeholder="confirm your password"
-              id="confirmPassword"
-           className={styles.inputbox} 
-             {...formik.getFieldProps('confirmPassword')}
-           />
-        <InputGroup.Text id='inputGroupPrepend'  onClick={(e) => {
-                    e.preventDefault()
-                    setToggleConfirmPassword(!toggleConfirmPassword)
-                  }}>
-                  {setToggleConfirmPassword ? (
-                    <AiOutlineEyeInvisible />
-                  ) : (
-                    <AiOutlineEye />
-                  )}
-                </InputGroup.Text>
-      
-               </InputGroup>
-               
-       {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-             <div className={styles.errorMsg}><MdErrorOutline className="h6 mt-1"/> {formik.errors.confirmPassword}</div>
-           ) : null}         
-         
-</div>
-     </Form.Group>
-     
-           <button className={styles.loginButton} 
-           type="submit" 
-        >
-           Submit
-           </button>
-           
-         </Form>
-       )}
-     </Formik>
-     
-     <hr/>
-     <div className={styles.semiGroup}>
-     <p className="text-center">
-      Don't Have an Account
-     </p>
-     <button className={styles.btnLink} onClick={handleSignup}>Signup</button>
-     </div>
-    
-          <ToastContainer position={'top-center'}>
+      {/*registration form*/}
+      <Formik
+        initialValues={{
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={Yup.object({
+          password: Yup.string()
+            .required("No new password provided.")
+            .min(8, "new Password is too short - should be 8 chars minimum.")
+            .matches(
+              /[a-zA-Z]/,
+              "new Password can only contain Latin letters."
+            ),
+          confirmPassword: Yup.string().when("password", {
+            is: (val) => (val && val.length > 0 ? true : false),
+            then: Yup.string().oneOf(
+              [Yup.ref("password")],
+              "New Password Must Match"
+            ),
+          }),
+        })}
+        onSubmit={(values) => {
+          navigate("/login");
+        }}
+      >
+        {(formik) => (
+          <Form noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
+            {/*pasword section*/}
+            <Form.Group className={styles.group}>
+              <div className={styles.subgroup}>
+                <Form.Label className={styles.labelfield}>Password</Form.Label>
+
+                <InputGroup className={styles.inputField}>
+                  <InputGroup.Text id="inputGroupPrepend">
+                    <RiLockPasswordFill />
+                  </InputGroup.Text>
+
+                  <Form.Control
+                    size="md"
+                    type={togglePassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter your new password"
+                    id="password"
+                    className={styles.inputbox}
+                    {...formik.getFieldProps("password")}
+                  />
+                  <InputGroup.Text
+                    id="inputGroupPrepend"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setTogglePassword(!togglePassword);
+                    }}
+                  >
+                    {togglePassword ? (
+                      <AiOutlineEyeInvisible />
+                    ) : (
+                      <AiOutlineEye />
+                    )}
+                  </InputGroup.Text>
+                </InputGroup>
+
+                {formik.touched.password && formik.errors.password ? (
+                  <div className={styles.errorMsg}>
+                    <MdErrorOutline className="h6 mt-1" />{" "}
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+            </Form.Group>
+
+            {/*confirm pasword section*/}
+            <Form.Group className={styles.group}>
+              <div className={styles.subgroup}>
+                <Form.Label className={styles.labelfield}>
+                  Confirm Password
+                </Form.Label>
+
+                <InputGroup className={styles.inputField}>
+                  <InputGroup.Text id="inputGroupPrepend">
+                    <RiLockPasswordFill />
+                  </InputGroup.Text>
+
+                  <Form.Control
+                    size="md"
+                    type={toggleConfirmPassword ? "text" : "password"}
+                    name="confirmPassword"
+                    placeholder="confirm your password"
+                    id="confirmPassword"
+                    className={styles.inputbox}
+                    {...formik.getFieldProps("confirmPassword")}
+                  />
+                  <InputGroup.Text
+                    id="inputGroupPrepend"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setToggleConfirmPassword(!toggleConfirmPassword);
+                    }}
+                  >
+                    {setToggleConfirmPassword ? (
+                      <AiOutlineEyeInvisible />
+                    ) : (
+                      <AiOutlineEye />
+                    )}
+                  </InputGroup.Text>
+                </InputGroup>
+
+                {formik.touched.confirmPassword &&
+                formik.errors.confirmPassword ? (
+                  <div className={styles.errorMsg}>
+                    <MdErrorOutline className="h6 mt-1" />{" "}
+                    {formik.errors.confirmPassword}
+                  </div>
+                ) : null}
+              </div>
+            </Form.Group>
+
+            <button className={styles.loginButton} type="submit">
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      <hr />
+      <div className={styles.semiGroup}>
+        <p className="text-center">Don't Have an Account</p>
+        <button className={styles.btnLink} onClick={handleSignup}>
+          Signup
+        </button>
+      </div>
+
+      <ToastContainer position={"top-center"}>
         <Toast
           bg={errorType}
           show={showToast}
           onClose={() => {
-            setShowToast(!showToast)
+            setShowToast(!showToast);
           }}
         >
           <Toast.Header>
             <img
-              src='holder.js/20x20?text=%20'
-              className='rounded me-2'
-              alt=''
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
             />
-            <strong className='me-auto'>BennyFoodie</strong>
+            <strong className="me-auto">BennyFoodie</strong>
           </Toast.Header>
-          <Toast.Body className='text-white'>{messageType}</Toast.Body>
+          <Toast.Body className="text-white">{messageType}</Toast.Body>
         </Toast>
       </ToastContainer>
     </Container>
