77ee661b18b3a8b98cfedf66b1ece975a7115929
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-02-22 15:14:14 +0100

    fix: product details image

--- src/components/foodMenu/allfoods.js
+++ src/components/foodMenu/allfoods.js
@@ -13,6 +13,7 @@ const allfoods = [
   image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721735/pngwing.com_16_zurgty.webp',
     name: 'Burger and Cola',
    price: 35,
+   desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 3,
@@ -20,6 +21,7 @@ const allfoods = [
    image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720496/pngwing.com-_8__sxocwn.webp',
     name: ' Mexico Salad Chicken',
     price: 40,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 4,
@@ -27,6 +29,7 @@ const allfoods = [
  image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721735/pngwing.com_19_c7ahlp.webp',
     name: 'RICE Skillet and Chicken',
     price: 70,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 5,
@@ -34,6 +37,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720837/pngwing.com_10_mexzxw.webp',
     name: 'Macaronni and Chicken Sauce',
     price: 65,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
     {
       id: 6,
@@ -41,6 +45,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721018/pngwing.com_11_u01hre.webp',
     name: 'French Fries',
     price: 30,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 7,
@@ -48,6 +53,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1675289357/pngwing.com_vnmees.png',
     name: 'Noodles and Egg',
     price: 20,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 8,
@@ -55,6 +61,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721887/pngwing.com_12_yac4gz.webp',
     name: 'Basmati Chinese Rice and Salad',
     price: 45,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 9,
@@ -62,6 +69,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673722321/pngwing.com_23_ujqxvg.png',
     name: 'Potato Porridge',
     price: 40,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 10,
@@ -69,6 +77,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673722305/pngwing.com_25_ybdmcr.png',
     name: 'Sour Chicken',
     price: 30,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
     {
       id: 11,
@@ -76,6 +85,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721735/pngwing.com_18_rgecze.webp',
     name: 'Salad Fried Potatoes',
     price:35,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
  {
       id: 12,
@@ -83,6 +93,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721735/pngwing.com_20_bm99pg.webp',
         name: 'Chicken Veggie Sauce',
     price: 40,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 13,
@@ -90,6 +101,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721735/pngwing.com_20_bm99pg.webp',
     name: 'DrumStick',
     price: 40,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 14,
@@ -97,6 +109,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721888/pngwing.com_14_rhjogg.webp',
     name: 'Americano Steak',
     price: 25,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
  {
       id: 15,
@@ -104,6 +117,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673721888/pngwing.com_13_gfuhxr.webp',
     name: 'Americano Salad Steak',
     price: 25,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 16,
@@ -111,6 +125,8 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720736/pngwing.com_9_a4g9pe.webp',
     name: 'Double Burger',
     price: 25,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
+    
   },
   {
       id: 17,
@@ -118,6 +134,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720496/pngwing.com-_8__sxocwn.webp',
     name: 'Vegge Salad Chicken',
     price:  29,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 18,
@@ -125,6 +142,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720365/pngwing.com-_7__emy3te.webp',
     name: 'Hot Dog Crunchies',
     price: 20,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 19,
@@ -132,6 +150,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720238/pngwing.com-_6__eix6nv.webp',
     name: 'Veggie Chicken Mixed Macaronni',
     price: 30,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
 {
       id: 20,
@@ -139,6 +158,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720157/pngwing.com-_5__ofmeey.webp',
     name: 'Omelette Paratti',
     price: 15,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 21,
@@ -146,6 +166,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673720016/pngwing.com-_4__affge7.webp',
     name: 'Omelette Salad',
     price: 15,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 22,
@@ -153,6 +174,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673719880/pngwing.com-_3__yja3r5.webp',
     name: 'Spaggheti Buronto',
     price: 15,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 23,
@@ -160,6 +182,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673719778/pngwing.com-_2__v944jl.webp',
     name: 'Salad Creamy',
     price: 30,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
   {
       id: 24,
@@ -167,6 +190,7 @@ const allfoods = [
     image: 'https://res.cloudinary.com/dlst0ec4h/image/upload/v1673632501/Pngtree_healthy_food_3776802_nqbeku.webp',
     name: 'Veggie Egg Macaronni',
     price: 35,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
     id: 25,
@@ -174,6 +198,7 @@ const allfoods = [
    image: "https://res.cloudinary.com/dlst0ec4h/image/upload/v1673701234/food-png-2957_so38ol.webp",
     name: "Italian Chicken Sauce",
     price: 20,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
     {
       id: 26,
@@ -181,13 +206,15 @@ const allfoods = [
   image: "https://res.cloudinary.com/dlst0ec4h/image/upload/v1673701487/fast-food-png-41613_rj94kr.webp",
     name: 'American Hamburger',
    price: 15,
+    desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   },
    {
       id: 27,
   category: 'Lunch',
-   image: "https://res.cloudinary.com/dlst0ec4h/image/upload/v1673701378/food-png-2962_voyv6e.webp",
+   image: "https://res.cloudinary.com/dlst0ec4h/image/upload/v1673701378/food-png-2962_voyv6e.webp", 
     name: 'European Cuisine Salad',
     price: 30,
+     desc: 'Slow-cooked for eight hours, this intensely flavored and juicy lamb shank, infused with fresh rosemary and sage, is a house specialty. Served with smooth mashed potatoes and char-grilled, crispy asparagus, topped with a tangy golden-brown glaze for a meal hard to forget.'
   }
   ]
   
--- src/pages/food-details/FoodDetails.jsx
+++ src/pages/food-details/FoodDetails.jsx
@@ -1,50 +1,59 @@
+import {useState, useEffect} from 'react'
 import "./FoodDetails.scss"
-import allfoods from "../../components/foodMenu/allfoods"
+import allmenu from "../../components/foodMenu/allfoods"
 import { useParams } from "react-router-dom"
 import HeaderSection from '../../components/headerSection/HeaderSection'
-import {Container, Row, Col} from "react-bootstrap"
-
-
-
+import {Container, Row, Col, Form, InputGroup} from "react-bootstrap"
 
 const FoodDetails = () =>{
+  const food = "https://res.cloudinary.com/dlst0ec4h/image/upload/v1675289357/pngwing.com_vnmees.png";
+  
+  const [tabs, setTabs] = useState('desc')
+  const {id} = useParams()
+  
+  const menu =  allmenu.find(menu => menu.id === id)
+  const [previewImg, setPreviewImg] =useState(allmenu.image)
+  
+  
   return(
-    <section className='FoodDetails.container'>
+    <section className='container-box'>
 <HeaderSection title='product 01'/>
 
-<container>
+<Container className="py-5">
 <Row>
 <Col lg='2' md='2'>
 <div className="product_images">
-<div className='img__item'>
-<img src='' alt='product images' className='w-100'/>
+<div className='img__item' onClick={() => setPreviewImg(menu.image)}>
+<img src={setPreviewImg} alt='product images' className='w-100'/>
 </div>
 
-<div className='img__item'>
-<img src='' alt='product images' className='w-100'/>
+<div className='img__item' onClick={() => setPreviewImg(menu.image)}>
+<img src={previewImg} alt='product images' className='w-100'/>
 </div>
 
-<div className='img__item'>
-<img src='' alt='product images' className='w-100'/>
+<div className='img__item' onClick={() => setPreviewImg(menu.image)}>
+<img src={previewImg} alt='product images' className='w-100'/>
 </div>
 </div>
 </Col>
 
 <Col lg='4' md='4'>
 <div className='product__main_img'>
-<img src='' alt='main img' className='w-100'/>
+<img src={previewImg} alt='main img' className='w-100'/>
 </div>
 </Col>
 
 <Col lg='6' md='6'>
 <div className='single_product-content'>
-<h2 className='product_title'>
+<h2 className='product_title mb-3'>
 chicken
 </h2>
-<span className='product_price'>
+<p className='product_price'> 
+{" "}
+Price: <span>
 30
-</span>
-<p>Category: <span> Breakfast</span></p>
+</span></p>
+<p className="category mb-5">Category: <span> Breakfast</span></p>
 
 <button className='btn'>
 Add to Cart
@@ -52,8 +61,99 @@ Add to Cart
 </div>
 </Col>
 
+<Col lg='12'>
+<div className='tabs d-flex align-items-center gap-3 py-2'>
+<h6 className='tab_active'>Description</h6>
+<h6>Review</h6>
+</div>
+
+<div className='tab_content'>
+<p>
+Noodles come to the table in many countries around the world, like glass noodles in China, soba noodles in Japan, and the scores of pasta types and shapes common in Italy. What makes North American noodles (found in or served with classic comfort foods like chicken noodles) different than most other noodles is that they are often enriched with egg or egg yolks. It's true that homemade noodles take more time to prepare than store-bought. But they reward cooks and their lucky guests with fresher, richer, better-tasting noodles to serve with all kinds of dishes (beef stroganoff, chicken noodle soup, and casseroles, to name a few). Once you learn how to make noodles, you'll find all kinds of satisfying ways to enjoy them. Fresh pasta can be cut long or short, thick or thin, but (thanks to the eggs) they're among the richest noodles around.
+</p>
+</div>
+
+<div className='tab_form mb-3'>
+<div className='review'>
+<p className='user_name mb-0'>
+williams blake
+</p>
+<p className='user_email mb-0'>
+jhnon01@gmail.user
+</p>
+<p className='review_text mt-2'>
+i love the food
+</p>
+</div>
+
+<div className='review'>
+<p className='user_name mb-0'>
+williams blake
+</p>
+<p className='user_email mb-0'>
+jhnon01@gmail.user
+</p>
+<p className='review_text mt-2'>
+i love the food
+</p>
+</div>
+
+<div className='review'>
+<p className='user_name mb-0'>
+williams blake
+</p>
+<p className='user_email mb-0'>
+jhnon01@gmail.user
+</p>
+<p className='review_text mt-2'>
+i love the food
+</p>
+</div>
+
+<Form className='form'>
+<Form.Group className='form_group'>
+<InputGroup>
+<Form.Control
+type='text'
+placeholder='Enter your Name'
+className="inputfield"/>
+</InputGroup>
+</Form.Group>
+
+<Form.Group className='form_group'>
+<InputGroup>
+<Form.Control
+type='email'
+className="inputfield"
+placeholder='Enter your Email'/>
+</InputGroup>
+</Form.Group>
+
+<Form.Group className='form_group'>
+<InputGroup>
+<Form.Control
+as='textarea'
+rows={6}
+type='text'
+className="inputfield"
+placeholder='Enter your Message'/>
+</InputGroup>
+</Form.Group>
+
+<button 
+type="submit" 
+className='btn'>
+submit
+</button>
+
+</Form>
+</div>
+
+
+</Col>
+
 </Row>
-</container>
+</Container>
     </section>
     
     )
